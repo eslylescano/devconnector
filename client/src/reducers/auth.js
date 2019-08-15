179f8371a1df7bff0a903433832871c6d1c63c8a
Esly Adán <eslylescano@gmail.com>
2019-08-15 02:04:46 +0100

    Logout & Nabbar Links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -28,7 +29,6 @@ export default function (state = initialState, action) {
 
         case REGISTER_SUCCESS:
         case LOGIN_SUCCESS:
-        case LOGIN_FAIL:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
@@ -39,6 +39,8 @@ export default function (state = initialState, action) {
 
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT:
             localStorage.removeItem('token');
             return {
                 ...state,
