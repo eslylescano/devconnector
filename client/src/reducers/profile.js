c5377005ee128b64d89fb30f3e14df3fcf54b331
Esly Adán <eslylescano@gmail.com>
2019-09-02 22:43:03 +0100

    Finish Profile Actions & Reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,10 @@
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  GET_REPOS,
+  UPDATE_PROFILE
+} from "../actions/types";
 const initialState = {
   profile: null,
   profiles: [],
@@ -18,11 +24,23 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
 
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     default:
