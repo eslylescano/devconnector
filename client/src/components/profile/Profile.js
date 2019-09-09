97b4507b37696020ab1fa8ad5c96b3e2e75f8a72
Esly Adán <eslylescano@gmail.com>
2019-09-09 21:47:15 +0100

    Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub.js";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
@@ -72,6 +73,9 @@ const Profile = ({
               )}
             </div>
           </div>
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername} />
+          )}
         </Fragment>
       )}
     </Fragment>
